05c123d5f63267aa9716f8f9112c839bf08c378b
sergeyzoloto <sergiozoloto@gmail.com>
2024-04-01 16:50:15 +0200

    bottom nav basic styling

--- client/src/components/BottomNav/BottomNav.jsx
+++ client/src/components/BottomNav/BottomNav.jsx
@@ -2,28 +2,39 @@ import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { GrBladesVertical, GrAdd, GrCalendar, GrList } from 'react-icons/gr';
 
+/* Styles */
+const styles = {
+  navButton: 'p-2 bg-blue-500 text-white',
+  navContainer:
+    'fixed bottom-0 w-full flex justify-center items-center bg-gray-200 p-4',
+};
+
+const routes = {
+  calendar: '/calendar',
+  tasks: '/tasks',
+};
+
+const iconSize = '1.5em';
+
 export const BottomNav = ({ toggleLeftSideMenu }) => {
   const location = useLocation();
-  const isCalendar = location.pathname === '/calendar';
+  const isCalendar = location.pathname === routes.calendar;
 
   return (
-    <div className="fixed bottom-0 w-full flex justify-center items-center bg-gray-200 p-4">
-      <button
-        onClick={toggleLeftSideMenu}
-        className="p-2 bg-blue-500 text-white"
-      >
-        <GrList />
+    <div className={styles.navContainer}>
+      <button onClick={toggleLeftSideMenu} className={styles.navButton}>
+        <GrList size={iconSize} />
       </button>
-      <button className="p-2 bg-blue-500 text-white">
-        <GrAdd />
+      <button className={styles.navButton}>
+        <GrAdd size={iconSize} />
       </button>
       {isCalendar ? (
-        <Link to="/tasks" className="p-2 bg-blue-500 text-white">
-          <GrBladesVertical />
+        <Link to={routes.tasks} className={styles.navButton}>
+          <GrBladesVertical size={iconSize} />
         </Link>
       ) : (
-        <Link to="/calendar" className="p-2 bg-blue-500 text-white">
-          <GrCalendar />
+        <Link to={routes.calendar} className={styles.navButton}>
+          <GrCalendar size={iconSize} />
         </Link>
       )}
     </div>
