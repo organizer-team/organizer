22ac534609cfbc6272bcf221f011c590298fdc66
sergeyzoloto <sergiozoloto@gmail.com>
2024-04-02 12:41:22 +0200

    popup toggle for add button

--- client/src/components/BottomNav/BottomNav.jsx
+++ client/src/components/BottomNav/BottomNav.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import { GrBladesVertical, GrAdd, GrCalendar, GrList } from 'react-icons/gr';
 import PropTypes from 'prop-types';
@@ -9,6 +9,8 @@ const styles = {
   navContainer:
     'fixed bottom-0 w-full flex justify-center items-center bg-gray-200 p-4',
   navWrapper: 'w-full flex justify-center',
+  popupButton: 'w-full bg-green-500 text-white rounded p-2 mb-2',
+  popupMenu: 'absolute bottom-full mb-2 w-32 bg-white shadow-lg rounded-lg p-2',
 };
 
 const routes = {
@@ -22,15 +24,31 @@ export const BottomNav = ({ toggleLeftSideMenu }) => {
   const location = useLocation();
   const isCalendar = location.pathname === routes.calendar;
 
+  const [showPopup, setShowPopup] = useState(false);
+
+  const togglePopup = () => {
+    setShowPopup(!showPopup);
+  };
+
   return (
     <div className={styles.navWrapper}>
       <div className={styles.navContainer}>
         <button onClick={toggleLeftSideMenu} className={styles.navButton}>
           <GrList size={iconSize} />
         </button>
-        <button className={styles.navButton}>
+        <button onClick={togglePopup} className={styles.navButton}>
           <GrAdd size={iconSize} />
         </button>
+        {showPopup && (
+          <div className={styles.popupMenu}>
+            <button onClick={togglePopup} className={styles.popupButton}>
+              Task
+            </button>
+            <button onClick={togglePopup} className={styles.popupButton}>
+              Event
+            </button>
+          </div>
+        )}
         {isCalendar ? (
           <Link to={routes.tasks} className={styles.navButton}>
             <GrBladesVertical size={iconSize} />
