e6611ded6e810dbede64175e9332e7642131b261
Ozgur Ozhan <ozgur2096@gmail.com>
2024-04-02 15:40:45 +0200

    custom colors added to tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primaryColor: '#9747FF',
+        lighterShade: '#B580FF',
+        darkerShade: '#6E24B1',
+        accentLightPurple: '#C996FF',
+        accentDarkPurple: '#7A1FC3',
+        neutralLightGray: '#F2F2F2',
+        neutralDarkGray: '#333333',
+      },
+    },
   },
   plugins: [],
 };
