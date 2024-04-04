2873c2ebae3c6081aa9ac741583adad0f5f159ff
Ozgur Ozhan <ozgur2096@gmail.com>
2024-04-04 11:43:41 +0200

    paths modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,21 +15,19 @@ import MainLayout from './layout/MainLayout';
 function App() {
   return (
     <>
-      <UserContextProvider>
-        <Routes>
-          <Route path='/email-validation' element={<EmailValidation />} />
-          <Route path='/' element={<MainLayout />}>
-            <Route path='/tasks' element={<Tasks />} />
-            <Route path='/calendar' element={<Calendar />} />
-          </Route>
-          <Route path='/user' element={<AuthLayout />}>
-            <Route index element={<Home />} />
-            <Route path='/user/list' element={<UserList />} />
-            <Route path='/user/signup' element={<CreateUser />} />
-            <Route path='/user/login' element={<LoginPage />} />
-          </Route>
-        </Routes>
-      </UserContextProvider>
+      <Routes>
+        <Route path='/email-validation' element={<EmailValidation />} />
+        <Route path='/' element={<MainLayout />}>
+          <Route index element={<Home />} />
+          <Route path='/tasks' element={<Tasks />} />
+          <Route path='/calendar' element={<Calendar />} />
+        </Route>
+        <Route path='/user' element={<AuthLayout />}>
+          <Route path='/user/list' element={<UserList />} />
+          <Route path='/user/signup' element={<CreateUser />} />
+          <Route path='/user/login' element={<LoginPage />} />
+        </Route>
+      </Routes>
     </>
   );
 }
--- client/src/layout/AuthLayout.jsx
+++ client/src/layout/AuthLayout.jsx
@@ -1,10 +1,8 @@
 import { Outlet } from 'react-router-dom';
-import Nav from '../components/Nav/Nav';
 
 export default function AuthLayout() {
   return (
     <main>
-      <Nav />
       <Outlet />
     </main>
   );
