8f8c88c1eef8738b99dca3733fef2aeb6d595c20
Ozgur Ozhan <ozgur2096@gmail.com>
2024-04-30 12:55:31 +0200

    the running period of cron changed

--- server/src/app.js
+++ server/src/app.js
@@ -29,8 +29,8 @@ import { deleteUserIfExpired } from './models/User.js';
 app.use(cookieParser());
 
 // We use cron to schedule tasks
-cron.schedule('00 * * * *', () => {
-  // running a task every hour
+cron.schedule('00 12 * * *', () => {
+  // running a task every day at 12:00
   deleteUserIfExpired();
 });
 
