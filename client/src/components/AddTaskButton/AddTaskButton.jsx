e4d46e1960b06e6c86b96882338c1eebeb8c658b
mo92othman <mo92othman@gmail.com>
2024-05-13 13:42:05 +0200

    fix the import and crash and add the list back to the list page

--- client/src/components/AddTaskButton/AddTaskButton.jsx
+++ client/src/components/AddTaskButton/AddTaskButton.jsx
@@ -9,7 +9,7 @@ const AddTaskButton = () => {
   return (
     <button
       onClick={handleClick}
-      className="fixed bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/2 bg-gradient-to-r from-emerald-500 to-blue-500 text-white px-4 py-2 rounded-full shadow-md"
+      className="fixed bottom-1/2 right-1/2 transform translate-x-1/2 translate-y-1/2 bg-gradient-to-r from-emerald-500 to-blue-500 text-white px-4 py-2 rounded-full shadow-md mt-10"
     >
       <img src={AddTaskIcon} alt="Add Task" className="w-6 h-6" />{' '}
       {/* Plus icon */}
