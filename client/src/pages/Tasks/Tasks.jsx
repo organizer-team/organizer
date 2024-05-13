1d86aac489a2856a9384e15885ee4e77e146344a
mo92othman <mo92othman@gmail.com>
2024-05-13 11:59:16 +0200

    added add-task button

--- client/src/pages/Tasks/Tasks.jsx
+++ client/src/pages/Tasks/Tasks.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import TEST_ID from './Tasks.testid.js';
 import TaskList from '../../components/TaskList/TaskList';
+import AddTaskButton from '../../components/AddTaskButton/AddTaskButton';
 
 const Tasks = () => {
   return (
     <div data-testid={TEST_ID.container} className="bg-blue-200">
       <TaskList />
+      <AddTaskButton />
     </div>
   );
 };
